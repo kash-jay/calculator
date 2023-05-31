08ba1d2349fef9b91fa3f6759c9821aa84e7aead
kp2681 <kp2681@nyu.edu>
2023-05-31 16:35:23 +0530

    Added divide button

---
+++ calc.js
@@ -0,0 +1,15 @@
+const keypad = document.querySelector('.keypad');
+
+function createKeypad(){
+    for (var i = 0; i<4; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (var j = 0; j<4; j++) {
+            // const
+        }
+    }
+    
+
+}
+
+// createKeypad();
