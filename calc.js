6c53f9ca1303ee4014eb747f81ce5a865da3932c
kp2681 <kp2681@nyu.edu>
2023-05-31 17:15:41 +0530

    Added left keypad buttons

--- calc.js
+++ calc.js
@@ -1,15 +1,17 @@
-const leftKeypad = document.querySelector('.keypad');
+const leftKeypad = document.querySelector('.left');
 
 function createKeypad(){
-    for (var i = 0; i<4; i++) {
+    for (var i = 7; i>0; i = i - 3) {
         const row = document.createElement('div');
         row.classList.add('row');
-        for (var j = 0; j<4; j++) {
-            // const
+        for (var j = 0; j<3; j++) {
+            const key = document.createElement('button');
+            key.classList.add('key');
+            key.textContent = i + j;
+            row.appendChild(key);
         }
+        leftKeypad.appendChild(row);
     }
-    
-
 }
 
-// createKeypad();
+createKeypad();
