7d47f1833625ec6326f7f3b635334d53d3361168
kp2681 <kp2681@nyu.edu>
2023-06-01 08:20:51 +0530

    Added functionality for one operation

--- calc.js
+++ calc.js
@@ -1,11 +1,21 @@
 var currVal = 0;
 var prevVal = 0;
-var currText = "0";
-var prevText = "0";
+var currText = "";
+var prevText = "";
+var valText = "";
+var decimal = false;
+var result = 0;
+var operation = "";
+
+const calcObject = {
+    'add': (num1, num2) => num1 + num2,
+    'sub': (num1, num2) => num1 - num2,
+    'mult': (num1, num2) => num1 * num2,
+    'div': (num1, num2) => num1 / num2,    
+}
 
 const upperDisplay = document.querySelector('.prev-text');
 const lowerDisplay = document.querySelector('.new-text');
-
 const leftKeypad = document.querySelector('.left');
 
 function updateDisplay() {
@@ -47,7 +57,63 @@ const keys = document.querySelectorAll('.key');
 
 function keyClick(event) {
     const key = event.target;
+    var char = key.textContent.charCodeAt(0);
     console.log(key.textContent);
+    if (char>47 && char<58){    //all digits
+        valText = valText + key.textContent;
+        currText = currText + key.textContent;
+        console.log("currText: " + currText);
+        currVal = parseFloat(valText);
+        console.log("prevVal: " + prevVal);
+        console.log("currVal: " + currVal);
+        updateDisplay();
+    } 
+    else if (char == 46 && !decimal) {    //decimal
+        decimal = true;
+        valText = valText + key.textContent;
+        currText = currText + key.textContent;
+        console.log("currText: " + currText);
+        currVal = parseFloat(valText);
+        console.log("prevVal: " + prevVal);
+        console.log("currVal: " + currVal);
+        updateDisplay();
+    }
+    // else if (char == 247) { //division
+    //     operation = 'div';
+    //     currText = currText + key.textContent;
+    //     console.log("currText: " + currText);
+    //     updateDisplay();
+    //     prevVal = currVal;
+    // }
+    // else if (char == 215) {
+    //     operation = 'mult';
+    //     currText = currText + key.textContent;
+    //     console.log("currText: " + currText);
+    //     updateDisplay();
+
+    // }
+    else if (char == 43) {
+        operation = 'add';
+        currText = currText + key.textContent;
+        console.log("currText: " + currText);
+        updateDisplay();
+        prevVal = currVal;
+        valText = "";
+    }
+    // else if (char == 8722) {
+    //     operation = 'sub';
+    //     currText = currText + key.textContent;
+    //     console.log("currText: " + currText);
+    //     updateDisplay();
+
+    // }
+    else if (char == 61) {
+        var result = calcObject[operation](prevVal, currVal);
+        prevText = currText;
+        prevVal = result;
+        currText = "" + result;
+        updateDisplay();
+    }
 }
 
 keys.forEach((key) => {
